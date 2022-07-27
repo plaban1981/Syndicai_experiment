ad223e5512f4547d3dc6d037931581b2664c9857
PLABAN NAYAK <plaban1981@users.noreply.github.com>
2022-07-27 19:01:32 +0530

    Update app.py
    commented the main code

--- Credit_card_fraud_detetion/app.py
+++ Credit_card_fraud_detetion/app.py
@@ -28,5 +28,5 @@ def predict():
     return classifier.predict(payload)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000)
